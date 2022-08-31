703df7ab80ed755e78110286a274c9e846fd13ce
cv-arun <93077669+cv-arun@users.noreply.github.com>
2022-08-31 17:45:33 +0530

    Update config.js

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -6,13 +6,13 @@ import 'firebase/firebase-firestore'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: API_key,
-    authDomain: "olx-clone-64684.firebaseapp.com",
-    projectId: "olx-clone-64684",
-    storageBucket: "olx-clone-64684.appspot.com",
-    messagingSenderId: "1073689592539",
-    appId: "1:1073689592539:web:fda5b3cb30f0370ca582fe",
-    measurementId: "G-5KH7DTKJRN"
+    apiKey:"API_key",
+    authDomain: "Auth domain",
+    projectId: "project id",
+    storageBucket: "storage bucket",
+    messagingSenderId: "mesaging id",
+    appId: "appid",
+    measurementId: "measurmentid"
   };
 
  export default firebase.initializeApp(firebaseConfig)
